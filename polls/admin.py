def05e36d5395d12ac7e2e92ef52d9f319ba2fbf
Manoj Mishra <manojmishra.deb@gmail.com>
2019-11-26 17:42:12 +0530

    refactor with generics

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Poll, Choice
 
-admin.site.register(Poll)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date', 'created_by')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Author', {'fields': ['created_by']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', PollList.as_view(), name='index'),
+    path('polls/', PollList.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:pk>/', PollDetail.as_view(), name='detail'),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='detail'),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
 ]
