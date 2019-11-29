a926dbecfccca74495c8b004263511bbff731e82
Manoj Mishra <manojmishra.deb@gmail.com>
2019-11-29 14:15:46 +0530

    refactor with viewsets & add authentication

--- polls/urls.py
+++ polls/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from rest_framework.routers import DefaultRouter
+
+from .apiviews import PollViewSet, ChoiceList, CreateVote, UserCreate, LoginView
 
 from . import views
 
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
+
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('polls/', PollList.as_view(), name='index'),
-    # ex: /polls/5/
-    path('polls/<int:pk>/', PollDetail.as_view(), name='detail'),
-    path("choices/", ChoiceList.as_view(), name="choice_list"),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
+
+urlpatterns += router.urls
